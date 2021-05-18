4ee792728b93da3e9aec81254c7639407f4cc91c
luizjedi <luiz.pepe@hotmail.com>
2021-05-18 20:54:50 -0300

    Criando projeto SpringBoot

---
+++ src/test/java/com/bestApps/FirstProjectSpring/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestApps.FirstProjectSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
