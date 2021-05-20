8bf718eaaee216449e55e0b575667a21e889f178
luizjedi <luiz.pepe@hotmail.com>
2021-05-20 18:13:41 -0300

    Subtotal & Total methods

--- src/main/java/com/bestApps/FirstProjectSpring/entities/OrderItem.java
+++ src/main/java/com/bestApps/FirstProjectSpring/entities/OrderItem.java
@@ -28,25 +28,24 @@ public class OrderItem implements Serializable {
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
 		id.setOrder(order);
-		id.setProduct(product); 
+		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -65,6 +64,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return quantity * price;
 	}
 
 	@Override
